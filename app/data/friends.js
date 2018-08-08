8a0dcf869ed6c058e2ac905f1a2fe15654a0bc62
mccaffertycr <mccaffertycr@gmail.com>
2018-08-08 19:38:39 -0400

    ready to deploy

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = 
 [{
     name:"Mr. Rogers",
-    photo:"https://www.google.com/search?q=mr+rogers&safe=off&rlz=1C1CHBF_enUS793US793&tbm=isch&source=iu&ictx=1&fir=ALaW6TNH8T9VJM%253A%252CJ239T6bjqgTXxM%252C_&usg=AFrqEzchY96zvKl3PGKbPpo0aSK4w7UofQ&sa=X&ved=2ahUKEwjYlYXsiNzcAhVCnlkKHc5EDJQQ_h0wG3oECAcQCw#imgrc=ALaW6TNH8T9VJM:",
+    photo:"http://www.gstatic.com/tv/thumb/persons/80634/80634_v9_ba.jpg",
     scores:[
         5,
         1,
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -7,7 +7,19 @@ module.exports = function(app) {
     });
 
     app.post('/api/friends', function(req, res) {
+        console.log(req.body);
+        var userScore = req.body.scores.reduce((a, b) => a + b, 0);
+        res.json(bestMatch(userScore));
         friendsData.push(req.body);
-    })
-    
+    });    
+
+    function bestMatch(score) {
+        for (var i = 0; i < friendsData.length; i++) {
+            var friendScore = friendsData[i].scores.reduce((a ,b) => a + b, 0);
+            if ((friendScore - score) <= 5 || (friendScore - score) >= -5) {
+                return friendsData[i];
+            } 
+        }
+    }
+
 }
